55b5a5d12b4d1002c579e7615bb20ff3eb1a183a
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-02-07 18:31:36 +0100

    First Lesson

---
+++ lekcja1.py
@@ -0,0 +1,18 @@
+# pierwszy komentarz
+
+#incjalizacja zmiennej
+
+a = 5   # zmienna całkowita typu int
+b = 5.25 # zmienna zmiennoprzecinkowa float
+napis = "Witaj Świecie"     # zmienna ciagu znaków typu str - string
+
+print(napis)   # wyswietlenie napisu lub zmiennej w konsoli
+print(a)
+print(b)
+print(napis)
+
+
+
+
+
+
