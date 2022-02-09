32dba7ae61b458169f04c48338336d6ab9fe4582
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-02-09 10:57:12 +0100

    Second Lesson

--- Lekcja2.py
+++ Lekcja2.py
@@ -1,16 +1,20 @@
 # wprowadzanie zmiennej liczbowej
-
-
-
+wprowadz = int(input("podaj zmienną od uzytkownika: "))
 
 #operatory  artmetyczne i logiczne
 
-
-
+# <= , >= , += - + * /
+  # and   or    not
 
 
 
 
 # instrukcja warunkowa if
+
+if(wprowadz>0):
+    print(wprowadz)
+else:
+    print("nie istnieje")
+
 
 
