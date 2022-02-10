3319b007f7651b98afda2d16fe31b96f0bf0adb7
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-02-10 12:03:00 +0100

    Third Lesson

--- Lekcja3.py
+++ Lekcja3.py
@@ -1,20 +1,13 @@
 # petla for
-
-
-
-
-
+for liczba in range(1,20+1):
+    print(liczba, end=" ")
 
 #istotne elementy pętli for oraz porównanie jej z innymi językami programowania
 
 
-
-
-
-
-
-
-
 # zagnieżdzenie w pętli for instrukcji warunkowej
+for zmienna in range(1,2000+1):
+    if(zmienna % 2 == 0 and zmienna % 5 ==0):
+        print(zmienna)
 
 
