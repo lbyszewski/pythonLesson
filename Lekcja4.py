0c16ac17f7574fbb207f8627544fe497f03816a7
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-02-16 11:11:53 +0100

    Other Lesson

--- Lekcja4.py
+++ Lekcja4.py
@@ -1,8 +1,29 @@
+
 #zagnieżdżenia w pętli for
+poczatek = int(input("podaj p: "))
+koniec = int(input("podaj k: "))
+dzielnik = int(input("podaj d: "))
 
 
+if(poczatek >0 and koniec >0 and dzielnik >0):
+    for liczby in range(poczatek,koniec+1):
+        if(liczby % dzielnik == 0):
+            print(liczby)
+
+for t in range(1,10+1):
+    print(t)
+    for w in range(1,10+1):
+        print(w)
 
 #pętla while
+
+c = int(input("podaj c: "))
+
+x = 1
+while(x<=c):
+    x*=2
+    print(x)
+
 
 
 
--- Lekcja5.py
+++ Lekcja5.py
@@ -1,7 +1,19 @@
 #funkcje - szkielet
+d = int(input("podaj d: "))
+g = int(input("podaj g: "))
+def dodawanie(d,g):
+    wynik = d * g
+    print(wynik)
+
+print(dodawanie(d,g))
 
 #rodzaje funkcji
 
+def dodawanie(d,g):
+    wynik = d * g
+    return wynik
+
+print(dodawanie(d,g))
 
 # funkcje anonmimowe
 
